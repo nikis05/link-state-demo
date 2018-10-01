65ccf52cb54f7420820b4f9de3bcaa8210dc419d
Nikita Isaev <nikis05@mail.ru>
2018-10-01 12:18:48 +0300

    Added comments

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ import resolvers from './resolvers';
 import UsersView from './UsersView';
 const data = require('./data.json');
 
+/* In Apollo Boost, apollo-link-state is included by default, so we can pass all
+ * its settings directly to ApolloClient. If you have a custom Apollo
+ * configuration you need to set up apollo-link-state as described in the docs:
+ *
+ */
+
 const client = new ApolloClient({
   clientState: {
-    defaults: data,
+    defaults: data, // Data from JSON
+
+    /* GraphQL schema definitions, they are not required, but may be useful for
+     * debugging and introspection
+     */
     typeDefs,
+
+    /* There are default resolvers that cover simple cases, but you can provide
+     * your own ones, see resolvers.js for more info
+     */
     resolvers
   }
 });
